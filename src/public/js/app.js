59aae771749092acbf16249e395c3ba3ba99cf81
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-10-05 17:32:28 +0200

    to switch camera

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,10 +13,12 @@ async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter(device => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
     cameras.forEach(camera => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if(currentCamera.label === camera.label) option.selected = true;
       camerasSelect.appendChild(option);
     })
   } catch(e) {
@@ -24,14 +26,23 @@ async function getCameras() {
   }
 }
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstraints = {
+    audio: true, 
+    video: { facingMode: "user" }
+  };
+  const cameraConstraints = {
+    audio: true, 
+    video: { deviceId: { exact: deviceId } }
+  };
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstraints
+    );
     myFace.srcObject = myStream;
-    await getCameras();
+    if(!deviceId) {
+      await getCameras();
+    }
   } catch(e) {
     console.log(e);
   }
@@ -47,7 +58,7 @@ function handleMuteClick() {
     muteBtn.innerText = "Unmute";
     muted = true;
   } else {
-    muteBtn.innerText = "Mute"
+    muteBtn.innerText = "Mute";
     muted = false;
   }
 }
@@ -57,17 +68,21 @@ function handleCameraClick() {
     .getVideoTracks()
     .forEach((track) => (track.enabled = !track.enabled));
   if(cameraOff) {
-    cameraBtn.innerText = "Turn Camera Off"
+    cameraBtn.innerText = "Turn Camera Off";
     cameraOff = false;
   } else {
-    cameraBtn.innerText = "Turn Camera On"
+    cameraBtn.innerText = "Turn Camera On";
     cameraOff = true;
   }
 }
 
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
-
+camerasSelect.addEventListener("input", handleCameraChange);
 
 
 //chat feature of socket.io
